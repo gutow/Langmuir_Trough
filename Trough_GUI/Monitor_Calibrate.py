928472196f784c4b380210fb139e9c86f7c5aa3a
Jonathan Gutow <gutow@uwosh.edu>
2023-01-05 15:30:18 -0600

    most barr control displays synced

--- Trough_GUI/Monitor_Calibrate.py
+++ Trough_GUI/Monitor_Calibrate.py
@@ -23,7 +23,7 @@ def Monitor_Setup_Trough(calibrations):
     """
     from ipywidgets import Layout, Box, HBox, VBox, GridBox, Tab, Accordion, \
         Dropdown, Label, Text, Button, Checkbox, FloatText, RadioButtons, \
-        BoundedIntText
+        BoundedIntText, BoundedFloatText
     from ipywidgets import HTML as richLabel
     from ipywidgets import HTMLMath as texLabel
     from IPython.display import display, HTML
@@ -102,11 +102,48 @@ def Monitor_Setup_Trough(calibrations):
                                               Bar_Area_per_Molec])],
                                layout=Layout(border="solid"))
     # Manual Barrier Control
+    Barr_Target_Percent = 100.0
+    def on_change_Barr_Units(change):
+        print(change)
+        if change['new'] == 'cm':
+            Barr_Target.min = calibrations.barriers.cal_apply(0.0,0.0)[0]
+            Barr_Target.max = calibrations.barriers.cal_apply(1.0, 0.0)[0]
+            Barr_Target.value = calibrations.barriers.cal_apply(Barr_Target_Percent/100,0.0)[0]
+        if change['new'] == 'cm^2':
+            temptarg = calibrations.barriers.cal_apply(Barr_Target_Percent/100,0.0)[0]*\
+                                float(Trough_Width.value) + float(Skimer_Correction.value)
+            max = calibrations.barriers.cal_apply(1.0, 0.0)[0]*float(Trough_Width.value) +\
+                              float(Skimer_Correction.value)
+            min = calibrations.barriers.cal_apply(0.0,0.0)[0]*float(Trough_Width.value) +\
+                              float(Skimer_Correction.value)
+            Barr_Target.max = max
+            Barr_Target.min = min
+            Barr_Target.value = temptarg
+        if change['new'] == 'Angstrom^2/molec':
+            # TODO
+            pass
+        if change['new'] == '% of max':
+            Barr_Target.min = 0.0
+            Barr_Target.max = 100.0
+            Barr_Target.value = Barr_Target_Percent
+        pass
+
     Barr_Units = Dropdown(description="Units",
                           options=["% of max", "cm", "cm^2",
                                      "Angstrom^2/molec"])
+
+    Barr_Units.observe(on_change_Barr_Units, names='value')
+
+    def on_change_Barr_Direction(changed):
+        if Barr_Direction.value == 'Move To':
+            Barr_Target.disabled = False
+        else:
+            Barr_Target.disabled = True
+        pass
+
     Barr_Direction = RadioButtons(options=["Open", "Close", "Move To"])
-    Barr_Target = FloatText(value=100.0, disabled=True)
+    Barr_Direction.observe(on_change_Barr_Direction, names='value')
+    Barr_Target = BoundedFloatText(value=Barr_Target_Percent, min=0.0, max=100.0, step=0.1, disabled=True)
     Barr_Speed = FloatText(description="Speed (/min)", value=100.0,
                            disabled=False)
     Barr_Start = Button(description="Start")
